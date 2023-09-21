5ab9ee9ec9b74233b5ed7e3b4c74bb24134eb419
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-09-21 22:31:41 +0300

    feat: Implemented header

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-import '../js/notifications.js';
-import '../js/menu.js';
 import '../js/custom-select.js';
-import '../js/modals.js';
-import '../js/input-code.js';
-
-const findDepositsButton = document.querySelector('.search .search__find-deposits');
-
-if (findDepositsButton) {
-  findDepositsButton.addEventListener('click', () => (window.location.href = './search.html'));
-}
