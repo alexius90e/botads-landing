add5723caf619944c8d0f4f694b237dee02cf3d1
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-09-24 00:18:34 +0300

    feat: implemented search bar section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 import '../js/custom-select.js';
+
+const searchBarSort = document.querySelector('.search-bar__sort');
+
+searchBarSort.addEventListener('click', (event) => {
+  event.target.classList.toggle('reverse');
+});
