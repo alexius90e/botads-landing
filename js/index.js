66945e2deb6312f300067c027758586a6736db9a
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-01 01:35:50 +0300

    feat: implemented price modal

--- js/index.js
+++ js/index.js
@@ -5,3 +5,30 @@ const searchBarSort = document.querySelector('.search-bar__sort');
 searchBarSort.addEventListener('click', (event) => {
   event.target.classList.toggle('reverse');
 });
+
+const priceModal = document.getElementById('priceModal');
+const priceMin = document.getElementById('priceMin');
+const priceMax = document.getElementById('priceMax');
+const priceButton = document.getElementById('priceButton');
+
+priceButton.addEventListener('click', () => priceModal.classList.remove('hide'));
+
+priceModal.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('modal__overlay')) priceModal.classList.add('hide');
+  if (target.classList.contains('modal__close')) priceModal.classList.add('hide');
+  if (target.classList.contains('price-modal__button_reset')) {
+    priceMin.value = '';
+    priceMax.value = '';
+  }
+  if (target.classList.contains('price-modal__button_apply')) {
+    const hasPriceLimits = priceMin.value || priceMax.value;
+    const priceButtonText = `от ${Number(priceMin.value)} ₽  до ${Number(priceMax.value)} ₽`;
+    priceButton.innerText = hasPriceLimits ? priceButtonText : 'Цена';
+    priceModal.classList.add('hide');
+  }
+});
+
+const dateButton = document.getElementById('dateButton');
+const recommendButton = document.getElementById('recommendButton');
+const usersButton = document.getElementById('usersButton');
