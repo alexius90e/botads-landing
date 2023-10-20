f170de7006727dcaf09d9a46aa1dfb5f9af39ca7
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-20 14:37:26 +0300

    feat: added filter search bar

--- js/index.js
+++ js/index.js
@@ -8,7 +8,17 @@ searchBarSort.addEventListener('click', (event) => {
   event.target.classList.toggle('reverse');
 });
 
-
-
 const recommendButton = document.getElementById('recommendButton');
 const usersButton = document.getElementById('usersButton');
+
+const filterSearchBar = document.querySelector('.filter__search-bar');
+const filterSearchButton = document.querySelector('.filter__search');
+
+filterSearchButton.addEventListener('click', () => {
+  filterSearchBar.classList.toggle('active');
+});
+
+filterSearchBar.addEventListener('click', (event) => {
+  const isCloseButton = event.target.classList.contains('filter__search-bar-back');
+  if (isCloseButton) filterSearchBar.classList.remove('active');
+});
